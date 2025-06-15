1199e912f4c025ddf3a758f2c7b4a08dba9147cc
theanxiuser <theanxiuser@gmail.com>
2025-06-15 08:30:08 +0545

    all files added

---
+++ PYTHON/greet.py
@@ -0,0 +1,10 @@
+def greet(name):
+    return f"Hello, {name}"
+
+
+# git config --global user.email <github email>
+# git config --global user.username <github username>
+# git config --global user.name <name>
+
+
+# git config --list
